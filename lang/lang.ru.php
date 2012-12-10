dd14b7c107a986f04fafd0679bfb9bc8d5edf3bb
alexbirukov <lord_maloy@mail.ru>
2012-12-10 23:22:01 +0600

    Add Russain language
    Add Russain language file

---
+++ lang/lang.ru.php
@@ -0,0 +1,21 @@
+<?php
+
+	$about = array(
+		'name' => 'Русский',
+		'author' => array(
+			'name' => 'Александр Бирюков',
+			'email' => 'info@alexbirukov.ru',
+			'website' => 'http://alexbirukov.ru'
+		),
+		'release-date' => '2012-06-18'
+	);
+
+	/**
+	 * Backbonejs
+	 */
+	$dictionary = array(
+
+		'Please make sure extension requirejs is loaded properly and its submodules are loaded as well. Try <code>git submodule add git://github/iwyg/requirejs extensions/requirejs --recursive</code> from within the symphony root path' => 
+		'Убедитесь, что у вас установлено и включено расширение requirejs. Попробуйте выполнить команду <code>git submodule add git://github/iwyg/requirejs extensions/requirejs --recursive</code> в корневом каталоге движка.',
+
+	);
